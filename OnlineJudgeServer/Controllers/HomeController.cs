32b7c03a978d46dcdbc5e3c0378cffbc640017af
小康 <xiaokang@cnblogs.com>
2019-06-01 18:34:40 +0800

    test

--- OnlineJudgeServer/Controllers/HomeController.cs
+++ OnlineJudgeServer/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OnlineJudgeServer.Models;
@@ -45,11 +46,102 @@ namespace OnlineJudgeServer.Controllers
             if (start == 0)
             {
                 result = _libraryContext.Books.Where(item => item.Title.Contains(keyword)).ToList();
+                
+                
             }
 
-            return Ok(result);
+            var res = new List<SampleBook>();
+
+            foreach (var x in result)
+            {
+                res.Add(new SampleBook
+                {
+                    author = Convert(x.Author),
+                    available_num = new Random().Next(60,80),
+                    binding = x.Binding,
+                    call_number = x.CallNumber,
+                    class_num = x.ClassNum,
+                    id =x.Id,
+                    imgs = new Image
+                    {
+                        large = "@string",
+                        medium = "@string",
+                        small = "https://www.library-online.cn/imgs/picture-ISBN-978-7-5004-9379-2.jpg"
+                    },
+                    isbn = long.Parse(x.Isbn),
+                    pages = x.Page,
+                    price = x.Price,
+                    pubdate = x.Pubdate.ToString(),
+                    publisher = x.Publisher,
+                    review_num = new Random().Next(80,100),
+                    title = x.Title,
+                    total_num = new Random().Next(80,100),
+                    total_score = new Random().NextDouble()*10,
+                    translator = new List<string>(),
+                    words = x.Word
+
+                });
+            }
+            return Ok(res);
         }
         
+        private List<string> Convert(string x)
+        {
+            var ans = new List<string>();
+            string s = "";
+            for (int i = 0; i < x.Length; i++)
+            {
+                
+                if (x[i] != ',' && x[i] != '"' && x[i] != '[' && x[i] != ']')
+                {
+                    s += x[i];
+                }
+                else
+                {
+                    if (s != "")
+                    {
+                        ans.Add(s);
+                        s = "";
+                    }
+                }
+            }
+
+            return ans;
+        }
+        
+    }
+
+    public class SampleBook
+    {
+        public List<string> author;
+        public int available_num;
+        public string binding;
+        public string call_number;
+        public string class_num;
+        public int id;
+        public Image imgs;
+
+        public long isbn;
+        public int pages;
+        public double price;
+        public string pubdate;
+        public string publisher;
+        public int review_num;
+
+        public string title;
+        public int total_num;
+        public double total_score;
+
+        public List<string> translator;
+
+        public int words;
+    }
+
+    public class Image
+    {
+        public string small;
+        public string medium;
+        public string large;
     }
 
     public class SearchInfo
