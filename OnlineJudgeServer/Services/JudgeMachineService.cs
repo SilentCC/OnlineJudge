86b33b3ea3f41df1acfc7ff591c3ee92eb36604c
小康 <xiaokang@cnblogs.com>
2019-07-09 11:29:32 +0800

    fix error

--- OnlineJudgeServer/Services/JudgeMachineService.cs
+++ OnlineJudgeServer/Services/JudgeMachineService.cs
@@ -102,7 +102,7 @@ namespace OnlineJudgeServer.Services
                         break;
                     }*/
 
-                    process.Close();
+                    //process.Close();
                 }
             }
 
