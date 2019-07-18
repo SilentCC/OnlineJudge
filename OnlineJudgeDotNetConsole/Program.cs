945c498b0049012765350edbb0228fa6a9e2ee8c
小康 <xiaokang@cnblogs.com>
2019-07-18 18:44:33 +0800

    Fix code type

--- OnlineJudgeDotNetConsole/Program.cs
+++ OnlineJudgeDotNetConsole/Program.cs
@@ -1,20 +1,19 @@
 using System;
 using System.Collections.Generic;
 
-namespace MyTestBenchMarks
+namespace MyProgram;
 {
-    class Program
+  class Program
+  {
+    static void Main()
     {
-        static void Main()
-        {
-            string line = Console.ReadLine();
+         string line = Console.ReadLine();
+         var str = line.Split(" ");
+         int a = int.Parse(str[0]);
+         int b = int.Parse(str[1]);
+         Console.WriteLine(a+b); 
 
-            var str = line.Split(" ");
-            
-            int a = Int32.Parse(str[0]);
-            int b = Int32.Parse(str[1]);
-            Console.WriteLine(a+b);
-        }
     }
+  }
 }
 
