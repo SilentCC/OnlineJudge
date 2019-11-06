2e4502c5aea1078b94cd2e59c09c4b383416bcd2
小康 <xiaokang@cnblogs.com>
2019-11-06 14:39:25 +0800

    add right access

--- OnlineJudgeServer/Controllers/SubmitsController.cs
+++ OnlineJudgeServer/Controllers/SubmitsController.cs
@@ -84,6 +84,7 @@ namespace OnlineJudgeServer.Controllers
                 result.Add(new
                 {
                     id = sub.SubmitId,
+                    problemId = sub.ProblemId,
                     title = problem.Title,
                     status = sub.JudgeResult,
                     userName = user.UserName,
